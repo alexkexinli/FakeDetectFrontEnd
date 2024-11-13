149125580729db8d84fd559d6d0ecd5107a2d411
alex <727891533@qq.com>
2024-11-13 20:44:40 +0800

    change

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import gradio as gr
 import requests
 
 def upload_and_predict(video):
+    print(video)
     if video is None:
         return "请上传一个视频文件。"
+    print(type(video))
     # 将视频文件发送到 FastAPI 接口
-    files = {'file': (video.name, video, 'video/mp4')}
-    response = requests.post("http://127.0.0.1:8000/detect", files=files)
+    # files = {'file': (video.name, video, 'video/mp4')}
+    with open(video, 'rb') as f:
+        files = {
+            'file': ('video.mp4', f, 'video/mp4')  # 'file' 为表单字段名，根据接口需求调整
+        }
+        response = requests.post("http://127.0.0.1:8000/detect2", files=files)
+
     if response.status_code == 200:
         result = response.json()
         return result
@@ -15,7 +22,7 @@ def upload_and_predict(video):
 
 iface = gr.Interface(
     fn=upload_and_predict,
-    inputs=gr.File(label="上传视频文件", type="filepath"),
+    inputs=gr.Video(label="上传视频文件",sources=["upload"]),
     outputs="text",
     title="视频人脸检测与推理",
     description="上传一个视频文件，系统将提取包含人脸的帧并进行推理。",
